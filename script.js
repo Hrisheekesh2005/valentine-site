f478094e44aae62a884d0d2551a23c29de6dab2c
HRISHEEKESH NARAYAN P E <hrisheekeshnarayan@gmail.com>
2026-03-14 07:36:46 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function yesPage(){
+
+for(let i=0;i<50;i++){
+
+let confetti=document.createElement("div");
+
+confetti.style.position="fixed";
+confetti.style.width="8px";
+confetti.style.height="8px";
+confetti.style.backgroundColor="white";
+confetti.style.left=Math.random()*100+"vw";
+confetti.style.top="-10px";
+confetti.style.opacity=1;
+confetti.style.transform="rotate("+Math.random()*360+"deg)";
+confetti.style.transition="transform 3s linear, top 3s linear";
+
+document.body.appendChild(confetti);
+
+setTimeout(()=>{
+confetti.style.top="100vh";
+confetti.style.transform="rotate(720deg)";
+},50);
+
+setTimeout(()=>{
+confetti.remove();
+},3000);
+
+}
+
+setTimeout(()=>{
 window.location.href="yes.html";
+},1500);
+
 }
 
 function nextPage(){
@@ -52,3 +83,20 @@ heart.remove();
 });
 
 });
+/* floating hearts generator */
+
+setInterval(()=>{
+
+let heart=document.createElement("div");
+heart.classList.add("heart-bg");
+
+heart.style.left=Math.random()*100+"vw";
+heart.style.animationDuration=(Math.random()*3+4)+"s";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},6000);
+
+},500);
